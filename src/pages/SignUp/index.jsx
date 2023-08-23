a2bb4e2282fc31150f041b994aff7276c318a54c
hellenvelasques <hgv92jc@gmail.com>
2023-08-23 09:24:41 -0300

    feat: criada tela de cadastro.

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,29 @@
+import { FiLock, FiMail, FiUser } from 'react-icons/fi'
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, Form, Background } from "./styles";
+
+export function SignUp() {
+
+  return (
+    <Container>
+      <Background />
+      <Form>
+        <h1>Rocketnotes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Crie sua conta</h2>
+
+        <Input type="text" placeholder="Nome" icon={FiUser} />
+        <Input type="text" placeholder="E-mail" icon={FiMail} />
+        <Input type="password" placeholder="Senha" icon={FiLock} />
+
+        <Button type="button" title="Cadastrar" />
+
+
+        <a href="#">Voltar para o login</a>
+      </Form>
+
+    </Container>
+  );
+}
